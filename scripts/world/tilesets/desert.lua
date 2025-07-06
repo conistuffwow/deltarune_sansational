76bc92109f8882dddf508fb788a253be35289528
conistuffwow <deatu013@gmail.com>
2025-07-05 22:39:53 -0400

    Initial commit

---
+++ scripts/world/tilesets/desert.lua
@@ -0,0 +1,32 @@
+return {
+  version = "1.11",
+  luaversion = "5.1",
+  tiledversion = "1.11.2",
+  name = "desert",
+  class = "",
+  tilewidth = 20,
+  tileheight = 20,
+  spacing = 0,
+  margin = 0,
+  columns = 16,
+  image = "../../../assets/sprites/tilesets/desert.png",
+  imagewidth = 320,
+  imageheight = 320,
+  transparentcolor = "#d77bba",
+  objectalignment = "unspecified",
+  tilerendersize = "grid",
+  fillmode = "stretch",
+  tileoffset = {
+    x = 0,
+    y = 0
+  },
+  grid = {
+    orientation = "orthogonal",
+    width = 20,
+    height = 20
+  },
+  properties = {},
+  wangsets = {},
+  tilecount = 256,
+  tiles = {}
+}
---
+++ scripts/world/tilesets/desert.tsx
@@ -0,0 +1,7 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.11" tiledversion="1.11.2" name="desert" tilewidth="20" tileheight="20" tilecount="256" columns="16" tilerendersize="grid">
+ <editorsettings>
+  <export target="desert.lua" format="lua"/>
+ </editorsettings>
+ <image source="../../../assets/sprites/tilesets/desert.png" trans="d77bba" width="320" height="320"/>
+</tileset>
